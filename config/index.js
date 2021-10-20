d1d8ea0a1e84cda16e2740620f5e2455381b53dd
HZ\houchen <houchen@tsign.cn>
2021-10-20 16:20:04 +0800

    修改静态资源路径

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'http://116.62.35.26/yd-vue',
 
     /**
      * Source Maps
